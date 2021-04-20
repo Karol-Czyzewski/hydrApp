da93680994af33c4228d903b5226724ac5d55245
Karol Czyżewski <Karolek0493@wp.pl>
2021-04-20 21:02:17 +0200

    add: listeners, localStorage, date of drink

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,27 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const counter = document.querySelector('.glass__counter-js');
+const addButton = document.querySelector('.button-add-js');
+const removeButton = document.querySelector('.button-remove-js');
+let number = 0;
+const key = new Date().toISOString().slice(0, 10)
+
+const getItems = localStorage.getItem(key);
+if (getItems) {
+    number = getItems;
+    counter.textContent = getItems;
+}
+
+addButton.addEventListener('click', () => {
+    counter.textContent = ++number;
+    localStorage.setItem(key, number)
+});
+
+removeButton.addEventListener('click', () => {
+    if (number === 0) { return }
+    else {
+        counter.textContent = --number;
+    }
+    localStorage.setItem(key, number)
+});
